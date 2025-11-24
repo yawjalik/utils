5e1065a3667fc6616ed97fcd68650590a8bf65ea
Jalik Yaw <jalik.yaw@planto.io>
2025-11-24 15:10:50 +0800

    yeehaw

---
+++ src/main/java/com/jyaw/CommonUtils.java
@@ -0,0 +1,12 @@
+package com.jyaw;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+@UtilityClass
+@Slf4j
+public class CommonUtils {
+    public void sayHello() {
+        log.info("Hello World!");
+    }
+}
